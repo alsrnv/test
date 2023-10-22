60a50e4c55c6650ccaeaf6eb7c5595f7d9834a0f
al.srnv <saranov92@gmail.com>
2023-10-22 16:14:07 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Генерация временного ряда
 timestamps = pd.date_range(start='2021-01-01', end='2021-01-31', freq='D')
@@ -11,11 +11,12 @@ sensor_names = ['Sensor_1'] * len(timestamps)
 # Создание DataFrame
 df = pd.DataFrame({'timestamp': timestamps, 'metric_value': metric_values, 'sensor_name': sensor_names})
 
-# Построение графика
-plt.figure(figsize=(10, 6))
-plt.plot(df['timestamp'], df['metric_value'], marker='o')
+# Построение графика с использованием Seaborn
+plt.figure(figsize=(12, 6))
+sns.lineplot(x='timestamp', y='metric_value', hue='sensor_name', data=df, marker='o')
 plt.title('Time Series of AI VR Calls')
 plt.xlabel('Timestamp')
 plt.ylabel('Metric Value')
 plt.grid(True)
+plt.legend(title='Sensor Name')
 plt.show()
