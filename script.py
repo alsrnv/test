716ab84062b8475a76f0cd494343f3e86e1b389b
al.srnv <saranov92@gmail.com>
2023-10-22 18:29:01 +0300

    Update script.py

--- script.py
+++ script.py
@@ -10,15 +10,22 @@ def analyze_sensors(df):
         mean_value = np.mean(df_sensor['metric_value'])
         std_value = np.std(df_sensor['metric_value'])
         
-        observed_values = df_sensor['metric_value'].value_counts().sort_index().values
-        unique_values = df_sensor['metric_value'].unique()
+        observed_freq = df_sensor['metric_value'].value_counts().sort_index()
+        unique_values = observed_freq.index
+        observed_values = observed_freq.values
         
         # Проверка распределения Пуассона
-        expected_poisson = np.array([poisson.pmf(int(x), mean_value) for x in unique_values]) * len(df_sensor)
+        expected_poisson = np.array([poisson.pmf(int(x), mean_value) * len(df_sensor) for x in unique_values])
+        
+        # Убедимся, что размеры совпадают
+        if len(observed_values) != len(expected_poisson):
+            print(f"Skipping {sensor} due to size mismatch")
+            continue
+        
         chi2_poisson, p_poisson = chisquare(f_obs=observed_values, f_exp=expected_poisson)
         
         # Проверка нормального распределения
-        expected_norm = np.array([norm.pdf(x, mean_value, std_value) for x in unique_values]) * len(df_sensor)
+        expected_norm = np.array([norm.pdf(x, mean_value, std_value) * len(df_sensor) for x in unique_values])
         chi2_norm, p_norm = chisquare(f_obs=observed_values, f_exp=expected_norm)
         
         # Определение типа распределения
