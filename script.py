f66df367f5f60e0c9fc2fcc01b6d2a1712b16ac4
al.srnv <saranov92@gmail.com>
2023-10-22 16:34:44 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,29 @@
+import pandas as pd
+from fbprophet import Prophet
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-# Построение графика с использованием Seaborn
-plt.figure(figsize=(12, 6))
-sns.lineplot(x='timestamp', y='metric_value', hue='sensor_name', data=df, marker='o')
-plt.title('Time Series of AI VR Calls')
-plt.xlabel('Timestamp')
-plt.ylabel('Metric Value')
-plt.grid(True)
-plt.legend(title='Sensor Name')
+# Предположим, что df - это ваш исходный DataFrame с колонками 'timestamp', 'metric_value', и 'sensor_name'
+
+# Шаг 1: Фильтрация данных для одного из sensor_name, например, 'Sensor_1'
+df_sensor = df[df['sensor_name'] == 'Sensor_1']
+
+# Шаг 2: Подготовка данных для Prophet
+df_prophet = df_sensor.rename(columns={'timestamp': 'ds', 'metric_value': 'y'})
+
+# Шаг 3: Инициализация и обучение модели
+model = Prophet()
+model.fit(df_prophet)
+
+# Шаг 4: Прогнозирование
+future = model.make_future_dataframe(periods=0)
+forecast = model.predict(future)
+
+# Определение выбросов
+forecast['anomaly'] = 0
+forecast.loc[forecast['yhat_lower'] > df_prophet['y'].reset_index(drop=True), 'anomaly'] = -1  # Ниже нижнего предела
+forecast.loc[forecast['yhat_upper'] < df_prophet['y'].reset_index(drop=True), 'anomaly'] = 1   # Выше верхнего предела
+
+# Визуализация результатов
+fig = model.plot(forecast)
+plt.title('Time Series with Anomalies')
 plt.show()
